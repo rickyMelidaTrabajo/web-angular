369112cbc4dfedf1989bbe8f85261120949c397d
Ricky Melida <ricardomelida92.trabajo@gmail.com>
2020-09-09 05:48:29 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent,
+    ProductsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
